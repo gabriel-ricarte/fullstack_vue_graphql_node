06484699f2d45dbfd2e9917fa8f07a1dd523d29b
Gabriel Ricarte <gabriel.ricarte@itarget.com.br>
2021-08-27 20:34:24 -0300

    começando o servidor com graphQl

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require ("apollo-server");
+
+const typeDefs = `
+
+    type Item{
+        id: Int
+        type: String
+        description: String
+    }
+
+    type Query{
+        prefixes: [Item]
+        sufixes: [Item]
+    }
+
+`;
+
+const resolvers = {
+    Query:{
+        prefixes(){
+            return [];
+        },
+        sufixes(){
+            return [];
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs , resolvers})
